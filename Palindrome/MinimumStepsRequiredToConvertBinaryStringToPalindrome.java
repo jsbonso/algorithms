3afb5a13327cd1ebc903e4ef2b83abb0db18610b
Jon Bonso <jsbonso@gmail.com>
2022-10-20 04:59:52 -0400

    Create MinimumStepsRequiredToConvertBinaryStringToPalindrome.java

---
+++ Palindrome/MinimumStepsRequiredToConvertBinaryStringToPalindrome.java
@@ -0,0 +1,39 @@
+
+/*
+    You are given a binary string, s, consisting of characters '0' and '1'. 
+    Transform this string into a palindrome by performing some operations. 
+    In one operation, swap any two characters, s[i] and s[j]
+    
+    Determine the minimum number of swaps required to make the string a palindrom.
+    If it is impossible to do so, then return -1.
+    
+    Note:
+    A palindrome is a string that reads the same backward as forward, for example, strings "0",
+    "111" 010 10101 are palindromes, but strings 001 10 11101 are note. 
+    
+    
+    Example: 
+    
+    Let string s = "0100101". 
+    The following shows the minimum number of steps required. It uses 1-based indexing.
+    
+    1. Swap characters with indices (4,5)
+    2. Swap characters with indices (1,2)
+    
+    Now, the binary string is now palindromic. 
+    The answer is 2 swaps.
+    
+    
+    
+*/
+
+public class MinimumStepsRequiredToConvertBinaryStringToPalindrome
+{
+    public static void main(String[] args) {
+        System.out.println("Welcome to Online IDE!! Happy Coding :)");
+    }
+    
+    public static int minSwapsRequired(String s){
+        return 
+    }
+}
