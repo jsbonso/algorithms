d7a4a54a984c25081eb2d78a61d19b88bdcc9f40
Jon Bonso <jsbonso@gmail.com>
2022-10-22 19:23:52 -0400

    Create ThreeSumSolutionNoSort.java

---
+++ TwoSum/ThreeSumSolutionNoSort.java
@@ -0,0 +1,72 @@
+/**
+
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+ 
+
+Example 1:
+
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Explanation: 
+nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+The distinct triplets are [-1,0,1] and [-1,-1,2].
+Notice that the order of the output and the order of the triplets does not matter.
+Example 2:
+
+Input: nums = [0,1,1]
+Output: []
+Explanation: The only possible triplet does not sum up to 0.
+Example 3:
+
+Input: nums = [0,0,0]
+Output: [[0,0,0]]
+Explanation: The only possible triplet sums up to 0.
+ 
+
+Constraints:
+
+3 <= nums.length <= 3000
+-105 <= nums[i] <= 105
+
+ */
+
+class ThreeSumSolutionNoSort {
+
+    /**
+    
+    - Input: unsorted
+    - Create a list of list of Integers
+    - Iterate the array
+        -
+    
+     */
+    public List<List<Integer>> threeSum(int[] nums) {
+
+        Set<List<Integer>> res = new HashSet<>();
+        Set<Integer> dups = new HashSet<>();
+        Map<Integer, Integer> seen = new HashMap<>();
+
+        for (int i = 0; i < nums.length; ++i)
+            // The "add" function return true if the element doesn't exist yet
+            if (dups.add(nums[i])) {
+                // the  nums[i] element is a brand new one
+                // Iterate through the array
+                for (int j = i + 1; j < nums.length; ++j) {
+                    int complement = -nums[i] - nums[j];
+                    if (seen.containsKey(complement) && seen.get(complement) == i) {
+                        List<Integer> triplet = Arrays.asList(nums[i], nums[j], complement);
+                        Collections.sort(triplet);
+                        res.add(triplet);
+                    }
+                    seen.put(nums[j], i);
+                }
+            }
+        return new ArrayList(res);
+        
+    }
+}
