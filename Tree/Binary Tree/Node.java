16d941f088f7664010e0b42a8d975564f895e423
Jon Bonso <jsbonso@gmail.com>
2022-10-24 08:22:27 -0400

    Create Node.java

---
+++ Tree/Binary Tree/Node.java
@@ -0,0 +1,19 @@
+/**
+       ROOT
+       / \
+      /   \
+   Left    Right 
+
+*/
+class Node{
+  
+  String value;
+  Node leftNode, rightNode;
+  
+  Node(String value){
+    this.value;
+    leftNode = null;
+    rightNode = null;
+  }
+  
+}
