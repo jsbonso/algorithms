814e7de9181e363b23977ec8105a21569ca82598
Jon Bonso <jsbonso@gmail.com>
2022-10-24 08:46:37 -0400

    Create Node.java

---
+++ Tree/N-ary Tree/Node.java
@@ -0,0 +1,32 @@
+/**
+
+  An N-Aary tree is composed of nodes where each node can have one or more children nodes (represented by "N")
+  
+        R O O T
+       /   |    \
+     /     |      \
+ child1  child2  child3
+ 
+ This class has <T> generics which can transform into the data type you specific
+*/
+
+import java.util.*;
+
+public class Node{
+
+  String value;
+  Node[] children;
+  
+  Node (String value){
+    this.value = data;
+    this.children = null;
+  }
+  
+  Node (String value, Node[] children){
+    this.value = data;
+    this.children = children;
+  }
+  
+
+  
+}
